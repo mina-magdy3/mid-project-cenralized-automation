64d5d869b069d01778bc8179952070c6229a566c
minamagdytawfik <20mina10magdy2002@gmail.com>
2026-05-25 17:04:26 +0300

    commit

--- .github/actions/react-docker-s2i/entrypoint.sh
+++ .github/actions/react-docker-s2i/entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -e
 
-
-# dockerd-entrypoint.sh > /tmp/dockerd.log 2>&1 &
-# until docker info >/dev/null 2>&1; do
-#   sleep 1
-# done
-
 REGISTRY=$INPUT_REGISTRY
 USERNAME=$INPUT_USERNAME
 PASSWORD=$INPUT_PASSWORD
@@ -17,6 +11,9 @@ FINAL_IMAGE="${REGISTRY}/${USERNAME}/${IMAGE_NAME}:${TAG}"
 
 echo "$PASSWORD" | docker login "$REGISTRY" -u "$USERNAME" --password-stdin
 
+pwd
+ls -la dist || echo "dist is missing"
+
 if [ ! -d "dist" ]; then
     echo "push_status=failed" >> "$GITHUB_OUTPUT"
     exit 1
@@ -24,7 +21,7 @@ fi
 
 docker build -f .github/actions/react-docker-s2i/Dockerfile.react -t "$FINAL_IMAGE" .
 
-if docker push "$FINAL_IMAGE:$TAG"; then
+if docker push "$FINAL_IMAGE"; then
     IMAGE_DIGEST=$(docker inspect --format='{{index .RepoDigests 0}}' "$FINAL_IMAGE" | cut -d'@' -f2)
     
     echo "image_name=${FINAL_IMAGE}" >> "$GITHUB_OUTPUT"
